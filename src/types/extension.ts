5310dcd5640d76a82f733639b9577b29cbe23aa3
Nathan Allen <nathan.noel.allen@gmail.com>
2025-01-30 01:44:44 -0500

    Initial commit

---
+++ src/types/extension.ts
@@ -0,0 +1,35 @@
+export type ChromeExtensionInfo = chrome.management.ExtensionInfo;
+
+// Our tracking type that extends Chrome's ExtensionInfo
+export interface TrackedExtensionInfo extends ChromeExtensionInfo {
+  lastUpdated: number;  // timestamp of last data update
+  isTracked: boolean;   // whether we're actively monitoring this extension
+}
+
+// Network request aggregation data structure
+export interface ExtensionNetworkStats {
+  extensionId: string;
+  totalRequests: number;
+  requestsByType: {
+    [key in chrome.webRequest.ResourceType]?: number;
+  };
+  requestsByDomain: {
+    [domain: string]: number;
+  };
+  lastRequest: number;      // timestamp of most recent request
+  firstTracked: number;     // timestamp when we started tracking
+}
+
+// Combined stats for storage
+export interface ExtensionStats {
+  extension: TrackedExtensionInfo;
+  network: ExtensionNetworkStats;
+}
+
+// For storing all extension data
+export interface ExtensionMonitorStore {
+  extensions: {
+    [extensionId: string]: ExtensionStats;
+  };
+  lastUpdated: number;
+}
